0b4636d78dee7750316abb14500d29bf5d2fb6b6
lordkign2 <Lordkign2@gmail.com>
2025-07-27 03:17:34 +0100

    local costomization

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [demoMode, setDemoMode] = useState(false);
